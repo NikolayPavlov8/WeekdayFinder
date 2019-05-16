69585a7f72b9afc1bb7dfafe57bfde3913ba952c
NikolayPavlov8 <nikolaspavlov8@gmail.com>
2019-05-16 15:58:17 +0300

    code1

---
+++ WeekdayFinder/WeekdayFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekdayFinder
+//
+//  Created by Nikolai Pavlov on 16/05/2019.
+//  Copyright © 2019 Nikolai Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
